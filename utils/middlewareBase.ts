cc1bb3b8bfb86a4e4d98009879e7c43d181562e0
RobotoSkunk <contact@robotoskunk.com>
2023-09-09 15:15:45 -0600

    Added correct license

--- utils/middlewareBase.ts
+++ utils/middlewareBase.ts
@@ -1,3 +1,10 @@
+/*
+	bnnuy - A fast and simple framework for Bun's HTTP API.
+	Copyright (C) 2023 Edgar Alexis Lima <contact@robotoskunk.com>
+	
+	Licensed under LGPLv3 <https://www.gnu.org/licenses/lgpl-3.0.txt>
+*/
+
 
 export default interface MiddlewareBase
 {
